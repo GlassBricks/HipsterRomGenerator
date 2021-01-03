0b817c3d9b1bc5f3cb7bd298e2dc443aaeda8c53
Benjamin Ye <24237065+enjoydambience@users.noreply.github.com>
2021-01-03 13:07:25 -0800

    Move knbt to separate module

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,39 +2,30 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.21"
-    kotlin("plugin.serialization") version "1.4.21"
-    application
+    kotlin("plugin.serialization") version "1.4.21" apply false
 }
 
-group = "me.ben"
-version = "1.0-SNAPSHOT"
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    implementation("com.github.ajalt.clikt:clikt:3.1.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.0.1")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-cbor:1.0.1")
-
-    testImplementation("io.kotest:kotest-runner-junit5:4.3.2")
-    testImplementation("io.kotest:kotest-assertions-core:4.3.2")
-    testImplementation("io.kotest:kotest-property:4.3.2")
-}
-
-tasks.test {
-    useJUnitPlatform()
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = "11"
-        freeCompilerArgs = listOf("-Xopt-in=kotlin.RequiresOptIn")
+allprojects {
+    repositories {
+        mavenCentral()
     }
 }
 
-application {
-    mainClass.set("me.glassbricks.romgenerator.MainKt")
+subprojects {
+    pluginManager.withPlugin("org.jetbrains.kotlin.jvm") {
+        dependencies {
+            testImplementation("io.kotest:kotest-runner-junit5:4.3.2")
+            testImplementation("io.kotest:kotest-assertions-core:4.3.2")
+            testImplementation("io.kotest:kotest-property:4.3.2")
+        }
+        tasks.test {
+            useJUnitPlatform()
+        }
+        tasks.withType<KotlinCompile> {
+            kotlinOptions {
+                jvmTarget = "11"
+                freeCompilerArgs = listOf("-Xopt-in=kotlin.RequiresOptIn")
+            }
+        }
+    }
 }
---
+++ knbt/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    kotlin("jvm")
+    kotlin("plugin.serialization")
+}
+
+repositories {
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.0.1")
+    testImplementation("io.kotest:kotest-runner-junit5:4.3.2")
+    testImplementation("io.kotest:kotest-assertions-core:4.3.2")
+    testImplementation("io.kotest:kotest-property:4.3.2")
+}
