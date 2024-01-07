6fb658d1fd1883080b95c7ed342cd80166638b5c
GlassBricks <24237065+GlassBricks@users.noreply.github.com>
2024-01-06 17:24:26 -0800

    Finish top; except at some point it breaks for some reason

--- romGenerator/scripts/doritodoor/top.kt
+++ romGenerator/scripts/doritodoor/top.kt
@@ -17,7 +17,7 @@ import ogMegafoldHipster.SequenceBuilder
 
 @Suppress("EnumEntryName")
 enum class TopMove {
-    wormb,
+    jworm,
     tobs,
     bfold, // bottom fold
     obs,
@@ -55,12 +55,13 @@ class TopSeq : SequenceBuilder<TopMove>() {
 
     private val wait get() = capsLock()
 
+    private val jworm get() = lower(TopMove.jworm)
+
     private val a get() = lower(TopMove.a)
     private val c get() = lower(TopMove.c)
     private val e get() = lower(TopMove.e)
     private val f get() = lower(TopMove.f)
 
-    private val b get() = add(TopMove.wormb)
     private val d get() = add(TopMove.d)
     private val g get() = add(TopMove.g)
 
@@ -107,7 +108,6 @@ class TopSeq : SequenceBuilder<TopMove>() {
     operator fun String.unaryPlus() = iterator().let {
         while (it.hasNext()) when (val ch = it.nextChar()) {
             'a' -> a
-            'b' -> b
             'c' -> c
             'd' -> d
             'e' -> e
@@ -131,11 +131,6 @@ class TopSeq : SequenceBuilder<TopMove>() {
     private fun stoGrab() {
         wait
         lower(TopMove.sto)
-    }
-
-    private fun stoPush() {
-        wait
-        upper(TopMove.sto)
     }
 
     private fun store(n: Int) {
@@ -170,7 +165,7 @@ class TopSeq : SequenceBuilder<TopMove>() {
     }
 
     private val maxPistonsOut = 6 // for layer 6 (index 5), we cheat a bit
-    private val pistonOut = BooleanArray(maxPistonsOut)
+    val pistonOut = BooleanArray(maxPistonsOut)
 
     // topmost folded pistons (layer 3-5 of pistonOut)
     // state machine!
@@ -235,10 +230,7 @@ class TopSeq : SequenceBuilder<TopMove>() {
         }
         val isStretch = layer <= 2 && (layer + 1..5).all { pistonOut[it] }
         when (layer) {
-            0 -> {
-                check(!isStretch) { "cannot extend layer 0 in stretch mode" }
-                e
-            }
+            0 -> e // for both normal and stretch
             1 -> {
                 if (!isStretch) {
                     // normal extension
@@ -272,24 +264,19 @@ class TopSeq : SequenceBuilder<TopMove>() {
     private fun removePistons() {
         val toRetract = pistonOut.indexOfFirst { it }
         if (toRetract == -1) error("No pistons to retract")
-
-        fun doRetract(startIndex: Int) {
-            for (i in startIndex downTo 0) when (i) {
+        val wasStretch = toRetract <= 2 && (toRetract..5).all { pistonOut[it] }
+        if (!wasStretch) {
+            for (i in toRetract downTo 0) when (i) {
                 0 -> E
                 1 -> +"Cd"
                 2 -> removeFold()
-                3, 4 -> {}
+                3, 4, 5 -> {}
                 else -> error("")
             }
-        }
-
-        val wasStretch = toRetract <= 2 && (toRetract..5).all { pistonOut[it] }
-        if (!wasStretch) {
-            doRetract(toRetract)
         } else when (toRetract) {
             0 -> +"e"
             1 -> +"dE"
-            2 -> doRetract(1)
+            2 -> +"CdE"
             else -> error("bad toRetract $toRetract")
         }
 
@@ -353,7 +340,7 @@ class TopSeq : SequenceBuilder<TopMove>() {
             return f
         }
         extend(n)
-        retract(n, isPiston = false, pistonsHigh = false)
+        retract(n, isPiston = false, isQc = false)
     }
 
     private fun extend(n: Int, origHeight: Int = n) {
@@ -366,53 +353,59 @@ class TopSeq : SequenceBuilder<TopMove>() {
     }
 
     private fun extendWithPistonsUp(n: Int, pistonsHigh: Boolean, origHeight: Int = n) {
-        when (n) {
-            1 -> {
+        when {
+            n == 1 -> {
                 if (!pistonsHigh) f
                 if (origHeight == 5) +"gg"
-                F
+                if (!pistonOut.all { it }) {
+                    F
+                } else {
+                    // use jank input
+                    check(nObsOut == 0 && n == origHeight)
+                    jworm
+                }
             }
-            2 -> {
+            n == 2 -> {
                 if (!pistonsHigh) f
                 // already double piston out
             }
-            3 -> {
+            n == 3 -> {
                 if (!pistonsHigh) {
-                    if (!(1..5).all { pistonOut[it] }) {
-                        addPistons(0)
+                    // double piston out
+                    addPistons(0)
+                    if ((1..5).all { pistonOut[it] }) {
+                        // use jank input instead of more pistons
+                        check(nObsOut == 0 && n == origHeight)
+                        jworm
                     } else {
-                        addObs()
                         +"ff"
-                        removeObs()
-                        // final strecth, we do weird stuff
-                        TODO("pow(3) stretch")
                     }
                 } else {
                     // already double piston out, see pull(2)
                     f
                 }
             }
-            4 -> {
+            n == 4 -> {
                 // spit pistons instead of obs first; less moves/parity issues
                 if (!pistonsHigh || n != origHeight) spit(2)
                 addObs()
             }
-            in 5..9 -> {
+            n in 5..9 && origHeight != 11 -> {
                 if (!pistonsHigh) f
                 addObs()
                 // for pistonsHigh && n==5, we don't do qc power because of parity issues
                 val obsHeight = if (pistonsHigh && n == origHeight && n > 5) n - 4 else n - 3
                 extend(obsHeight, origHeight)
             }
-            10, 11 -> {
+            origHeight in 10..11 -> {
+                // almost same as n==10..11, but also captures n==7 from 11 extension
+                // note n==3 captures
                 if (!pistonsHigh) {
                     // we don't have enough obs to do full extension, so spit first
                     spit(n - 2)
-                    addObs()
-                    extend(n - 4, origHeight = n)
-                } else {
-                    TODO("pow($n, true)")
-                }
+                } else check(n == 10)
+                addObs()
+                extend(n - 4, origHeight)
             }
             else -> TODO()
         }
@@ -450,24 +443,29 @@ class TopSeq : SequenceBuilder<TopMove>() {
                 // pistons are high regardless
                 +"gg"
             }
+            11 -> {
+                check(!pistonsHigh)
+                repeat(4) { g }
+            }
             else -> TODO("pow($n)")
         }
     }
 
-    private fun maybeRetractObs(n: Int, pistonsHigh: Boolean) {
+    private fun maybeRetractObs(n: Int, isQc: Boolean) {
         when (n) {
             in 0..3 -> {}
             4 -> removeObs() // obs already up
-            in 5..10 -> {
-                val obsHeight = if (pistonsHigh && n > 5 || n >= 10) n - 4 else n - 3
-                retract(obsHeight, isPiston = false, pistonsHigh = false)
+            in 5..11 -> {
+                // exception n==5, not actually qc
+                val obsHeight = if (isQc && n > 5 || n >= 10) n - 4 else n - 3
+                retract(obsHeight, isPiston = false, isQc = n == 11)
                 removeObs()
             }
             else -> TODO("retractObs($n)")
         }
     }
 
-    private fun retract(n: Int, isPiston: Boolean, pistonsHigh: Boolean) {
+    private fun retract(n: Int, isPiston: Boolean, isQc: Boolean) {
         require(n >= 0)
         if (n == 0) {
             // base case (spe), do nothing
@@ -481,9 +479,15 @@ class TopSeq : SequenceBuilder<TopMove>() {
             return
         }
 
-        maybeRetractObs(n, pistonsHigh)
+        maybeRetractObs(n, isQc)
         // pull topmost piston down
         pull(n - 2, false)
+        if (n == 11) {
+            // un-jank the pistonOut state
+            check(pistonOut[5] && (0..4).none { pistonOut[it] })
+            pistonOut[5] = false
+            pistonOut[4] = true
+        }
         // do previous row, with pistons already up
         extendWithPistonsUp(n - 1, true)
         retract(n - 1, isPiston, true)
@@ -493,7 +497,7 @@ class TopSeq : SequenceBuilder<TopMove>() {
         pull(n, true)
     }
 
-    private fun pull(n: Int, isSpit: Boolean) {
+    fun pull(n: Int, isSpit: Boolean) {
         if (n == 0) {
             // base case, do nothing. entendWithPistonsUp(1) will handle rest
             return
@@ -522,7 +526,7 @@ class TopSeq : SequenceBuilder<TopMove>() {
             extend(pistonRow)
         }
 
-        retract(pistonRow, isPiston = true, pistonsHigh = false)
+        retract(pistonRow, isPiston = true, isQc = false)
         removePistons()
     }
 }
@@ -547,9 +551,22 @@ fun SchemFile.modifyTopRom() {
 class GenBottom : StringSpec({
     "gen opening" {
         val seq = TopSeq().apply { opening() }.build()
-        println(seq.joinToString(" "))
+//        println(seq.joinToString(" "))
+        println(seq.size)
         val rom = encodeSimpleChungusRom(seq, encoding, topRomRestrictions)
         val schem = rom.toSchem(cartRotation = 90F).apply { modifyTopRom() }
+        schem.writeTo("top-opening")
+
+        tryTransfer("20htriangle/roms")
+    }
+    "gen just row 11" {
+        val seq = TopSeq().apply {
+            row(11)
+            checkStableState()
+        }.build()
+        val rom = encodeSimpleChungusRom(seq, encoding, topRomRestrictions)
+        val schem = rom.toSchem(cartRotation = 90F).apply { modifyTopRom() }
+//        schem.writeTo("row-11")
         schem.writeTo("top-opening")
 
         tryTransfer("20htriangle/roms")
@@ -557,8 +574,17 @@ class GenBottom : StringSpec({
 
     "print row" {
         val seq = TopSeq().apply {
-            row(11)
+            row(3)
             checkStableState()
+        }.build()
+        println(seq.joinToString(" "))
+        seq.forEach(::println)
+    }
+
+    "print pull 3 when jank" {
+        val seq = TopSeq().apply {
+            for (i in 2..5) pistonOut[i] = true
+            pull(3, false)
         }.build()
         println(seq.joinToString(" "))
         seq.forEach(::println)
